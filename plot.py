4dfd1f9175ea6242d86261da0ad0e585d0464b3a
maggz69 <swagzmania@gmail.com>
2018-04-19 20:37:36 +0200

    update plot.py
    Added comments to explain the functioning of the plot function.
    Added a print line that tells python to move to the next line once it's done printing one row.
    Removed unnecessary white space after the code.

--- plot.py
+++ plot.py
@@ -2,9 +2,15 @@ import math #all a0rbitary functions
 function = input("Enter a function f(x):\n")
 for y in range(10,-11,-1):
   for x in range(-10,11):
+    # eval calculates the function as it was inputted, 
+    # also necessary for this to begin the if condition since 
+    # when it comes to printing on the axes, 
+    # the other conditions might suffice but we need an 'o' there instead of a dash
     if y == round(eval(function)):
         print("o", sep="", end= "")    
     elif y == 0 and x != 0:
+      # instead of a dash, because this is the axis, 
+      # utilize the hyphen instead to make the points equidistant from the axes
       print("-", sep="", end= "")
     elif y == 0 and x == 0:
       print("+", sep="", end= "")
@@ -12,18 +18,4 @@ for y in range(10,-11,-1):
         print("|", sep="", end= "")
     else:
       print(" ", sep="", end= "")
-  print("\n",end="") #no newline space
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  print("") #move to the next line in the Y axis
